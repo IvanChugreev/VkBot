66c516e4059a687bcd6aa7be7a67afa8130f6da8
Петров Дима <Super.egas2015@gmail.com>
2022-12-16 00:19:00 +0300

    Interface implementation

--- VkBot/Program.cs
+++ VkBot/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Threading.Tasks;
 using TypesUsedByBot;
 using VkApiAdapterForBot;
+using DataBase;
 
 namespace VkBot
 {
@@ -10,15 +11,18 @@ namespace VkBot
     {
         async static Task Main(string[] args)
         {
+
             // Чтобы подключиться к VkApi в файле setting.txt должно быть: в первой строке токен, во второй строке id группы
             string[] setting = File.ReadAllText("setting.txt").Split(new char[] { '\n', '\r', ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
             // Инициализация API мессенджера
-            IMessengerApi<long> vkApi = new VkApiAdapter(setting[0], ulong.Parse(setting[1]));
+            //IMessengerApi<long> vkApi = new VkApiAdapter(setting[0], ulong.Parse(setting[1]));
 
             // Инициализация системы хранения
 
             //BotCommands<long> commands = new BotCommands<long>(vkApi, )
+
+            
         }
     }
 }
